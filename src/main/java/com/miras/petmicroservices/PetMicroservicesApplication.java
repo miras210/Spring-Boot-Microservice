787c1dbe4b8b1f8867524ce006eff06a5f032c0f
miras alimov <sektor.miras@gmail.com>
2022-04-03 03:39:50 +0600

    init

---
+++ src/main/java/com/miras/petmicroservices/PetMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.miras.petmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miras/petmicroservices/PetMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miras.petmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
